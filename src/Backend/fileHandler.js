6ba69555840ce07c612b7912d1e0af0ee5f28375
Lucas Zhang <lhzhang@live.unc.edu>
2020-12-22 17:16:57 -0500

    Switched up file checks for Series and CT

--- src/Backend/fileHandler.js
+++ src/Backend/fileHandler.js
@@ -45,6 +45,10 @@ export async function readRS(absDir) {
 
 export async function readCT(absDir) {
     let rawCT = await getFiles(absDir, 'CT');
+    if (rawCT.length <= 0) {
+        alert("Please make sure to include CT Images in the directory");
+        return {};
+    }
     let builtCT = await ctWorker.buildCT(rawCT);
     return builtCT;
 }
@@ -52,17 +56,13 @@ export async function readCT(absDir) {
 export async function readSeries(absDir) {
 
     let ctImages = await readCT(absDir);
-    if (ctImages.length <= 0) {
-        alert("Please make sure to include CT Images in the directory");
-        return {};
-    }
     let builtSeries = await seriesWorker.buildSeries(ctImages);
     let wrappedSeries = Factory.createWrapper(builtSeries, 'SERIES');
-    if (wrappedSeries.isSeries()) return wrappedSeries;
-    else {
+    if (!wrappedSeries.isSeries()) {
         alert("Please check that your CT images are all from the same series");
         return {};
-    };
+    } 
+    return wrappedSeries;
 }
 
 function parseFileName(filename) {
