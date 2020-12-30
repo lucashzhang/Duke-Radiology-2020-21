51426600656120a82869223ac40ad005f72ca6a3
Lucas Zhang <lhzhang@live.unc.edu>
2020-12-30 11:27:10 -0500

    Updated packages

--- public/electron.js
+++ public/electron.js
@@ -6,7 +6,15 @@ const isDev = require("electron-is-dev");
 let mainWindow;
 
 function createWindow() { 
-    mainWindow = new BrowserWindow({ width: 900, height: 680 }); 
+    mainWindow = new BrowserWindow({ 
+        width: 900, 
+        height: 680,
+        webPreferences: {
+            nodeIntegration: true, // <--- flag
+            // nodeIntegrationInWorker: true, // <---  for web workers
+            enableRemoteModule: true
+        }
+    }); 
     mainWindow.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`); 
     mainWindow.on("closed", () => (mainWindow = null)); 
 } 
